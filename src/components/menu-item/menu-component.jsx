bf119550cd8cd44c4a9429a3e8a7028a88a03e31
Oyesh <oyeshsin@hotmail.com>
2020-12-20 23:12:38 -0500

    Added react-router-dom

--- src/components/menu-item/menu-component.jsx
+++ src/components/menu-item/menu-component.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size}) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match}) => (
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div
       className='background-image'
       style={{
@@ -16,4 +17,5 @@ const MenuItem = ({ title, imageUrl, size}) => (
   </div>
 );
 
-export default MenuItem;
+// To access the history
+export default withRouter(MenuItem);
